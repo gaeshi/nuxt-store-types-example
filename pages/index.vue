f99cc2ff0aa3e20e144e8ba27c9abfc157b3627c
Viktors Garkavijs <gaeshi@gmail.com>
2020-03-27 18:53:23 +0900

    :fire: Remove generated code
    The removed code is from https://vuetifyjs.com/en/examples/layouts/google-youtube/

--- pages/index.vue
+++ pages/index.vue
@@ -10,26 +10,6 @@ import SearchHistory from '@/components/SearchHistory'
 export default {
   components: {
     SearchHistory
-  },
-  data: () => ({
-    drawer: null,
-    items: [
-      { icon: 'mdi-trending-up', text: 'Most Popular' },
-      { icon: 'mdi-youtube-subscription', text: 'Subscriptions' },
-      { icon: 'mdi-history', text: 'History' },
-      { icon: 'mdi-playlist-play', text: 'Playlists' },
-      { icon: 'mdi-clock', text: 'Watch Later' }
-    ],
-    items2: [
-      { picture: 28, text: 'Joseph' },
-      { picture: 38, text: 'Apple' },
-      { picture: 48, text: 'Xbox Ahoy' },
-      { picture: 58, text: 'Nokia' },
-      { picture: 78, text: 'MKBHD' }
-    ]
-  }),
-  created() {
-    this.$vuetify.theme.dark = true
   }
 }
 </script>
